1456478f80c6a39cd58c6de26ba9d130df15469d
Lucas <lvalandrorocha@hotmail.com>
2019-08-28 10:38:23 -0300

    Fix 1-D implementation

--- juliaset/1D_parallel_julia.c
+++ juliaset/1D_parallel_julia.c
@@ -30,16 +30,17 @@ int main(int argc, char *argv[]) {
     int width = 2 * n;
     int heigth = n;
     int rows = n / size;
-    int pixels_size = heigth * width * 3;
+    int rgb_pixels = 3;
+    int pixels_size = width * rows * rgb_pixels;
     float tint_bias = 1.0;
     unsigned char *pixels_row = malloc(sizeof(char) * pixels_size);
 
     int initial_heigth_pos = (((rank) % size) * rows);
     int last_heigth_pos = (((rank + 1) == size) ? (size * rows) : (((rank + 1) % size) * rows));
 
-    for (int y = initial_heigth_pos; y < last_heigth_pos; y++) {
-        for (int x = 0; x < width; x++) {
-            int result = compute_julia_pixel(x, y, width, heigth, tint_bias, &pixels_row[x * n + y]);
+    for (int x = 0; x < width; x++) {
+        for (int y = initial_heigth_pos, row = 0; y < last_heigth_pos && row < rows; y++, row++) {
+            int result = compute_julia_pixel(x, y, width, heigth, tint_bias, &pixels_row[rgb_pixels * ((row * width) + x)]);
         }
     }
 
@@ -58,21 +59,12 @@ int main(int argc, char *argv[]) {
         MPI_Send(message, sizeof(message), MPI_BYTE, 1, 1, MPI_COMM_WORLD);
     } else {
         MPI_Recv(buffer, sizeof(buffer), MPI_BYTE, rank - 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        // Open and read all pixels saved
-        unsigned char *buffer = malloc(sizeof(char) * pixels_size);
-        get_pixels_file(buffer, pixels_size, "julia1d.bpm");
-        // Merge the file pixels with the pixels_row that was calculated by this process     
-        unsigned char* pixels_result = add_pixels(buffer, pixels_row, pixels_size);
-        // Open file for saving the new pixels merged
+        // Open and save all pixels at EOF
         FILE *fp;
-        fp = fopen("julia1d.bpm", "w");
-        // Rewrite BPM Header
-        int res = write_bmp_header(fp, width, heigth);
-        fwrite(pixels_result, sizeof(char), pixels_size, fp);
+        fp = fopen("julia1d.bpm", "a");
+        fwrite(pixels_row, sizeof(char), pixels_size, fp);
 
         // Free pointers
-        free(pixels_result);
-        free(buffer);
         fclose(fp);
         // The last process doesnt send an MPI Message
         if (!is_last_process(rank, size)) {
